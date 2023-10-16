6c8efafdf617f7998b35e54297f2073b293c38d0
OĞULCAN DOĞAN <ogulcan.dgn@hotmail.com>
2023-10-16 20:52:17 +0300

    Create context

--- src/App.js
+++ src/App.js
@@ -1,34 +1,7 @@
-import Dropdown from "./components/Dropdown";
-import { useState } from "react";
+import DropdownPages from "./pages/DropdownPages";
 
 function App() {
-  const [selection, setSelected] = useState(null);
-
-  //başta null olan selected'ı optionlardan birine tıkladığımız zaman içini doldurur
-  const handleSelectedClick = (option) => {
-    setSelected(option);
-  };
-
-  const options = [
-    { label: "Red", value: "red" },
-    { label: "Green", value: "green" },
-    { label: "Blue", value: "blue" },
-  ];
-
-  return (
-    <div className="flex">
-      <Dropdown
-        options={options}
-        value={selection}
-        onChange={handleSelectedClick}
-      />
-      <Dropdown
-        options={options}
-        value={selection}
-        onChange={handleSelectedClick}
-      />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
---
+++ src/pages/DropdownPages.js
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import Dropdown from "../components/Dropdown";
+
+function DropdownPages() {
+  const [selection, setSelected] = useState(null);
+
+  //başta null olan selected'ı optionlardan birine tıkladığımız zaman içini doldurur
+  const handleSelectedClick = (option) => {
+    setSelected(option);
+  };
+
+  const options = [
+    { label: "Red", value: "red" },
+    { label: "Green", value: "green" },
+    { label: "Blue", value: "blue" },
+  ];
+
+  return (
+    <div className="flex">
+      <Dropdown
+        options={options}
+        value={selection}
+        onChange={handleSelectedClick}
+      />
+      <Dropdown
+        options={options}
+        value={selection}
+        onChange={handleSelectedClick}
+      />
+    </div>
+  );
+}
+
+export default DropdownPages;
