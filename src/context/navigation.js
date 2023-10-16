a32c8993734e1f0e5cee524cdac4b770c1734bfe
OĞULCAN DOĞAN <ogulcan.dgn@hotmail.com>
2023-10-16 21:19:12 +0300

    create currentPath

--- src/context/navigation.js
+++ src/context/navigation.js
@@ -1,10 +1,29 @@
-import { createContext } from "react";
+import { createContext, useState, useEffect } from "react";
 
 const NavigationContext = createContext();
 
 function NavigationProvider({ children }) {
-  return <NavigationContext.Provider value={}>{children}</NavigationContext.Provider>;
+  //*aktive path
+  const [currentPath, setCurrentPath] = useState(window.location.pathname);
+
+  useEffect(() => {
+    const handler = () => {
+      setCurrentPath(window.location.pathname);
+    };
+    window.addEventListener("popstate", handler);
+
+    return () => {
+      window.removeEventListener("popstate", handler);
+    };
+  }, []);
+
+  return (
+    <NavigationContext.Provider value={{}}>
+      {currentPath}
+      {children}
+    </NavigationContext.Provider>
+  );
 }
 
-export {NavigationProvider}
+export { NavigationProvider };
 export default NavigationContext;
