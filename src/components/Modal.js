b02a89c81bf17d0703587a5683f99181c0e64b07
OĞULCAN DOĞAN <ogulcan.dgn@hotmail.com>
2023-10-29 01:28:04 +0300

    create react portal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,11 +1,14 @@
+import ReactDOM from "react-dom";
+
 function Modal() {
-  return (
+  return ReactDOM.createPortal(
     <div>
       {/* inset-0 : bütün sayfayı kaplar */}
       <div className="absolute inset-0 bg-gray-300 opacity-80"></div>
       {/* burası modal */}
       <div className="absolute inset-80 p-10 bg-white">This a modal!</div>
-    </div>
+    </div>,
+    document.querySelector(".modal-container")
   );
 }
 
