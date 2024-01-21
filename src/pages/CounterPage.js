8ea5ab7b808c49def745a4e7a136ff412259a7fd
OĞULCAN DOĞAN <ogulcan.dgn@hotmail.com>
2024-01-21 21:27:54 +0300

    create form

--- src/pages/CounterPage.js
+++ src/pages/CounterPage.js
@@ -1,19 +1,37 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Button from "../components/Button";
 //use-counter hookunu burada kullandık
-import useCounter from "../hooks/use-counter";
+import Panel from "../components/Panel";
 
 //yukarıdaki useCounter function'unu altta kullanıyoruz
 function CounterPage({ initialCount }) {
-  const { count, increment, descrement, reset } = useCounter({ initialCount });
+  const [count, setCount] = useState(initialCount);
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
+  const increment = () => {
+    setCount(count + 1);
+  };
 
   return (
-    <div>
-      <h1>Count is {count}</h1>
-      <Button onClick={increment}>increment</Button>
-      <Button onClick={descrement}>descrement</Button>
-      <Button onClick={reset}>reset</Button>
-    </div>
+    <Panel className="m-3">
+      <h1 className="text-lg">Count is {count}</h1>
+      <div className="flex flex-row">
+        <Button onClick={increment}>increment</Button>
+        <Button onClick={decrement}>decrement</Button>
+      </div>
+
+      <form>
+        <label>Add a lot!</label>
+        <input
+          type="number"
+          className="p-1 m-3 bg-gray-50 border border-gray-300"
+        />
+        <Button>Add it!</Button>
+      </form>
+    </Panel>
   );
 }
 
